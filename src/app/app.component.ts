83221e7bec6874ece1380bbe567e8897528292e2
massimo <massipetra@hotmail.it>
2019-04-29 11:03:08 +0200

    interface added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IUser } from './models/user.model';
+
+
 
 @Component({
   selector: 'app-root',
@@ -10,36 +10,20 @@ import { IUser } from './models/user.model';
 export class AppComponent implements OnInit {
  
   header = 'To do list'
-  url = 'http://localhost:3000/employees';
-  users: IUser[];
-  name: string;
-  salary: string;
+
+ 
   
-  constructor(private http: HttpClient){}
+  constructor(){}
 
   ngOnInit() {}
   
-  getData() {
-   this.http.get(this.url).subscribe((users: IUser[]) => {
-     this.users = users
-     console.log(this.users);
-   });
-  }
+ 
   
-  postUser(user: IUser) {
-    this.http.post('http://localhost:3000/employees', user).subscribe(data => {
-      console.log(data)
-    })
-  }
 
-  createUser() {
-    const user: IUser = {name: this.name, salary: this.salary}
-    this.postUser(user);
-  }
+
+
   
-  deleteUser(user: IUser) {
-    this.http.delete('http://localhost:3000/employees/18').subscribe(data =>{console.log(data)});
-  }
+
 
 }
 
