7fb02c438562a0f11332b1fe71cde439fa4fa074
massimo <massipetra@hotmail.it>
2019-05-02 18:05:51 +0200

    HttpClient imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Item} from './item.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'to-do-list';
+export class AppComponent implements OnInit {
+  header = 'To do list !';
+  items: Item[];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){}
+
+  getData(){
+
+  }
 }
